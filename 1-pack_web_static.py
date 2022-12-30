afdb6b1b455c9f1d1c2dc0462130f36ee4721008
IamLam163 <olamidealiyu@gmail.com>
2022-12-30 06:22:18 +0100

    tgz file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder using the function do_pack
+"""
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    local('mkdir -p versions')
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "web_static_{}.tgz".format(time)
+    fname = "versions/{}".format(name)
+    archive_path = local("tar -cvzf {} web_static/".format(fname))
+    if archive_path:
+        return (archive_path)
+    else:
+        return None
