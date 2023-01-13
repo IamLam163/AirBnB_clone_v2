fd1fd3826c99b49b00ac3c4a18af7ae69e676e3c
IamLam163 <olamidealiyu@gmail.com>
2023-01-13 16:27:36 +0100

    documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+"""A script that displays Hello HBNB!"""
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """returns the output"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
