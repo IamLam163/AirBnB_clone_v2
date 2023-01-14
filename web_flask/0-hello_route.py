005a76e6ced8c42df9e321b1bb7e501321a28dff
IamLam163 <olamidealiyu@gmail.com>
2023-01-14 05:21:52 +0100

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,8 @@ def hello_hbnb():
     """returns the output"""
     return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
 
+if __name__ == '__main__':
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0")
 
