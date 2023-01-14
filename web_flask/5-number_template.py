92e9195790886ed44f484202cd337f617630af8d
IamLam163 <olamidealiyu@gmail.com>
2023-01-14 16:21:09 +0100

    more fixes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,55 +1,47 @@
 #!/usr/bin/python3
-"""Starts Flask web app
-Routes:
-    / - display "Hello HBNB!"
-    /hbnb - display "HBNB"
-    /c/<text> - display "C <text>"
-    /python/<text> - display "Python is cool"
-    /number/<n> - display n if integer
-    /number_template/<n> - display a HTML page if n is int
-"""
-from flask import Flask, render_template
+""" Starts a Flask web application """
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_route():
-    """prints Hello HBNB"""
-    return 'Hello HBNB!'
+def hello_hbn():
+    """ Returns Hello HBNB! from 0.0.0.0:5000 """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """prints HBNB"""
-    return 'HBNB'
+    """ Returns HBNB from 0.0.0.0:5000/hbnb """
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """prints C followed by <text> content"""
-    text = text.replace('_', " ")
-    return 'C {}'.format(text)
+def c_route(text):
+    """ Script displays the value of text"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/<string:text>', strict_slashes=False)
-def python(text="is cool"):
-    """prints Python is cool"""
-    text = text.replace('_', " ")
-    return "Python %s" % text
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """method displays text"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_n(n):
-    """checks if argument passed is and interger"""
-    return "%i is a number" % n
+def num_route(n):
+    """Return n if n is an int"""
+    return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """display a HTML page if n is int"""
-    return render_template('5-number.html', n=n)
-
+@app.route('/number_template/<n>', strict_slashes=False)
+def num_template(n):
+    """method renders an HTML page"""
+    return render_template("5-number.html", n=n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
