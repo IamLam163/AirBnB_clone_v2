94f87aa0f0515263e84f5743b80f1658e0c21fbb
IamLam163 <olamidealiyu@gmail.com>
2023-01-14 06:57:33 +0100

    fixed

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def num_route(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def num_template(n):
     """Returns an html page"""
     return render_template("5-number.html", num=n)
