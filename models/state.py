35adf33dbaeefeb8c057e5e42b2ba36d8cc9173e
IamLam163 <olamidealiyu@gmail.com>
2023-01-16 21:59:02 +0100

    pycode

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
@@ -15,11 +16,9 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
         # cities = relationship('City')
         cities = relationship('City', cascade="all, delete", backref='state')
-       # user = relationship("User", back_populates="addresses")
     else:
         @property
         def cities(self):
-
             '''returns the list of City instances with state_id
                 equals the current State.id
                 FileStorage relationship between State and City
@@ -40,18 +39,24 @@ class State(BaseModel, Base):
             """
 
             '''
-            cities = storage.all(City).items() # gets the entire storage- a dictionary
-            for city in cities.values(): # cities.value returns list of the city objects
-                if city.state_id == self.id: # if the object.state_id == self.id
-                    related_cities.append(city) # append to the cities list
+            cities = storage.all(City).items()
+            # gets the entire storage- a dictionary
+            for city in cities.values():
+                    # cities.value returns list of the city objects
+                if city.state_id == self.id:
+                        # if the object.state_id == self.id
+                    related_cities.append(city)
+                    # append to the cities list
             return related_cities
                 # state = relationship('State')
-                #the above block of code is buggy because, we use the items() on 43
-                #which returns a dict with a key and value pair but we return just the
+                #the above block of code is buggy because,
+                we use the items() on 43
+                #which returns a dict with a key and
+                value pair but we return just the
                 #key and do not return the value of the key
             '''
 
-            #another implementation of lines 43 to 47
+            # another implementation of lines 43 to 47
             cities = storage.all(City)
             for key in cities.keys():
                 if cities[key].state_id == self.id:
