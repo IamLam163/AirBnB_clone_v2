d537e7994d72cd216d5a8f3d61f0e67197fb3d28
IamLam163 <olamidealiyu@gmail.com>
2023-01-16 23:48:01 +0100

    pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,7 @@ from models import storage
 app = Flask(__name__)
 """flask app"""
 
+
 @app.route('/states_list', strict_slashes=False)
 def get_list():
     """displays a list of all states"""
@@ -19,6 +20,7 @@ def get_list():
 def tear_down(exception):
     """Method close the db session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
